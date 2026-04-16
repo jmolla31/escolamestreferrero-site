0d5f19cdd777e784c66ef0c10bb0cd872aa9fe47
jmolla31 <javimollamico@gmail.com>
2026-04-16 19:36:25 +0200

    Add mdx and pdf empedding support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [mdx()],
   i18n: {
     defaultLocale: 'ca',
     locales: ['ca', 'es'],
